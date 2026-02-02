efef6478db7102c6bc68273d56167a9b4e097209
Thinh53124 <thinh531240958@gmail.com>
2026-02-02 07:32:40 +0700

    Update minitestb2.js

--- Minitest/minitestb2.js
+++ Minitest/minitestb2.js
@@ -1,11 +1,16 @@
-let number = 50
-
-if (n%3 == 0) {
-    document.writeln("Fizz")
-} else if (n%5 == 0){
-    document.writeln("Buzz")
-} else if (n%3 == 0 && n%5 == 0) {
-    document.writeln("FizzBuzz")
-} else {
-    document.writeln(number);
+let n ;
+let sum = 0;
+for (n = 1; n<=50; n++){
+    if (n%3==0&&n%5==0){
+        console.log("FizzBuzz");
+        continue;
+    }else if(n%3==0){
+        console.log("Fizz");
+        sum+=n
+    }else if(n%5==0){
+        console.log("Buzz");
+    }else{
+        console.log(n);   
+    }
 }
+console.log(sum);
